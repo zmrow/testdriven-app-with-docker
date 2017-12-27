8c3353c790792605e2314c9d6515a0f17902d2da
zmrowicki@hotmail.com <zmrowicki@hotmail.com>
2017-12-26 21:04:08 -0800

    Initial commit.  Instantiate user dir and initial __init__

---
+++ users-service/project/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# routes
+@app.route('/users/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })
+
+
