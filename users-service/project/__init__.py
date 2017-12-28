9ab1a70c621e8f1196d228b3c68f083420d4a965
zmrowicki@hotmail.com <zmrowicki@hotmail.com>
2017-12-28 08:54:07 -0800

    Add postgres container: Add db folder to users-service with Dockerfile and create_sql.  Update Dockerfile-dev to ensure that users service depends on postgres container.  Add entrypoint script to users container to ensure that postgres db is up.

--- users-service/project/__init__.py
+++ users-service/project/__init__.py
@@ -1,5 +1,7 @@
 import os
+import datetime
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 # Instantiate the app
 app = Flask(__name__)
@@ -7,6 +9,21 @@ app = Flask(__name__)
 # Set config
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+
+# Instantiate the db
+db = SQLAlchemy(app)
+
+# Model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 # Routes
 @app.route('/users/ping', methods=['GET'])
--- users-service/project/config.py
+++ users-service/project/config.py
@@ -1,24 +1,28 @@
 # config for the users app
+import os
 
 
 class BaseConfig:
     '''Base config'''
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     '''Dev config'''
     DEBUG = True
-    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
     '''Testing config'''
     DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
 class ProductionConfig(BaseConfig):
     '''Prod config'''
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
