0edf18d38aa0acbd1e4be192db12824c2ba3025d
zmrowicki@hotmail.com <zmrowicki@hotmail.com>
2017-12-28 08:09:41 -0800

    Update requirements.txt syntax error (= to ==).  Update users-service/__init__.py to pull config from env vars.  Create Dockerfile-dev for users_service.  Add docker-compose-dev.yml to create the machine.

--- users-service/project/__init__.py
+++ users-service/project/__init__.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask, jsonify
 
 # Instantiate the app
 app = Flask(__name__)
 
 # Set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 # Routes
 @app.route('/users/ping', methods=['GET'])
