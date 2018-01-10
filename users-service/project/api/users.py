a1aa23d9475b6b49db5195666848ec180756a30b
Zac <zmrowicki@hotmail.com>
2018-01-09 16:01:56 -0800

    Add first template and update blueprint.  Add route handler for /. Add tests for said actions

--- users-service/project/api/users.py
+++ users-service/project/api/users.py
@@ -1,10 +1,15 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, render_template
 from project.api.models import User
 from project import db
 
 from sqlalchemy import exc
 
-users_blueprint = Blueprint('users', __name__)
+users_blueprint = Blueprint('users', __name__, template_folder='./templates')
+
+@users_blueprint.route('/', methods=['GET'])
+def index():
+    users = User.query.all()
+    return render_template('index.html', users=users)
 
 @users_blueprint.route('/users/ping', methods=['GET'])
 def ping_pong():
--- users-service/project/tests/test_users.py
+++ users-service/project/tests/test_users.py
@@ -132,5 +132,24 @@ class TestUserService(BaseTestCase):
             self.assertIn('fletcher@realpython.com', data['data']['users'][1]['email'])
             self.assertIn('success', data['status'])
 
+    def test_main_no_users(self):
+        '''Ensure the main route behaves correctly when no users have been added to the database'''
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<h1>All Users</h1>', response.data)
+        self.assertIn(b'<p>No users!</p>', response.data)
+
+    def test_main_with_users(self):
+        '''Ensure the main route behaves correctly when users have been added to the database'''
+        add_user('michael', 'michael@realpython.com')
+        add_user('fletcher', 'fletcher@realpython.com')
+        with self.client:
+            response = self.client.get('/')
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'<h1>All Users</h1>', response.data)
+            self.assertNotIn(b'<p>No users!</p>', response.data)
+            self.assertIn(b'michael', response.data)
+            self.assertIn(b'fletcher', response.data)
+
 if __name__ == '__main__':
     unittest.main()
